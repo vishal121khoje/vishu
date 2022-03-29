ac802a2388fc1401589b8afbbba8ac899fa4a9a6
vishal121khoje <101632771+vishal121khoje@users.noreply.github.com>
2022-03-29 03:58:54 -0700

    Add files via upload

---
+++ ForHelloMessage.java
@@ -0,0 +1,16 @@
+package Day4;
+
+public class ForHelloMessage {
+
+	public static void main(String[] args) {
+  //for(initialization  ;condition ; increment/decrement)
+ 
+	//for(int i=1;i<=10;i++) 
+		for(int i=10;i>=1;i--)
+	{
+		 System.out.println("hello"); 
+	
+	}
+	}
+
+}
